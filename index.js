65d1e838acbec2974b30bd38880137a00f429956
israks <nayime@hotmail.fr>
2025-02-15 03:10:46 +0100

    Premier ajout des fichiers

---
+++ index.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+app.whenReady().then(() => {
+  mainWindow = new BrowserWindow({
+    width: 350,
+    height: 450,
+    //frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadFile('index.html');
+});
